67a11f3f09e6074e7cd86a023578bebd912f4877
Xavatt <xavierguinto3@outlook.es>
2018-09-04 09:31:13 -0500

    First lines to start to read the file

--- MemoryScheduler.c
+++ MemoryScheduler.c
@@ -11,3 +11,25 @@
 #define OFFSET 9        /* Constant used to define the offset used */
 #define TLBSIZE 8       /* Constant for the TLB size */
 #define NUMARAMS 2      /* Constant used to define the number of parameters we must receive */
+
+/* Used to get a number from a file passed as a parameter */
+int GetInt(FILE *fp)
+{
+    int c,i; /* Character read and integer representation of it */
+    int sign = 1;
+    do
+    {
+        c = getc(fp); /* Get next character */
+        if(c == '#') /* Help to skip the comment's */
+        {
+           do
+           {
+               c = getc(fp);
+           } while(c != '\n');
+           if(c == '-')
+           {
+               sign = -1;
+           }
+        }
+    }while (!isdigit(c) && !feof(fp));
+}
