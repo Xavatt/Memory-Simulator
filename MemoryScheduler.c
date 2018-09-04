69af8419e0b5d05ede81741b06d33425be849422
Xavatt <xavierguinto3@outlook.es>
2018-09-04 09:24:23 -0500

    Definition of Global Constant Values that will be used

--- MemoryScheduler.c
+++ MemoryScheduler.c
@@ -1,4 +1,13 @@
-#include <ctype.h>      // Used for the isdigit() function
-#include <stdlib.h>     // Used for malloc definition
-#include <stdio.h>      // Used for fprintf
+#include <ctype.h>      /* Used for the isdigit() function */
+#include <stdlib.h>     /* Used for malloc definition */
+#include <stdio.h>      /* Used for fprintf */
 #include <math.h>
+
+/********************************************************
+ *              Global Constant Values                  *
+ ********************************************************/
+#define LOGADD 16       /* Constant used to define the logial address space */
+#define PHYADD 13       /* Constant used to define the physical address space */
+#define OFFSET 9        /* Constant used to define the offset used */
+#define TLBSIZE 8       /* Constant for the TLB size */
+#define NUMARAMS 2      /* Constant used to define the number of parameters we must receive */
